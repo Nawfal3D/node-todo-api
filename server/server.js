fcca2b940e7ac38adb4fa6fbf3f311255702127c
Nawfal Al Hashimy <nawfal@ecs-mac-mar-4.home>
2018-01-26 14:59:41 +0000

    Add get todos id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID } = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
@@ -31,7 +32,27 @@ app.get('/todos',(req,res)=>{
   });
 });
 
-app.listen(3002, ()=>{
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+      return res.status(404).send(console.log('ID not valid'));
+      };
+
+  Todo.findById(id).then((todo)=>{
+
+    if (!todo) {
+      return res.status(404).send(console.log('todo not found'))
+      };
+res.send({todo});
+  }
+).catch((e)=>{res.status(400).send()})
+
+});
+
+
+
+
+app.listen(3000, ()=>{
   console.log(`Started on port 3000`);
 });
 
