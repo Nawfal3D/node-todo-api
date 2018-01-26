4c2884707dfcf37fd1fdcee7940ad9d1c4402c6e
Nawfal Al Hashimy <nawfal@ecs-mac-mar-4.home>
2018-01-26 18:56:19 +0000

    setup app for heroku - corrected mongo db error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_6cz96bd9:isc4bqm3j3j01kjf8em9o8mjr6@ds117148.mlab.com:17148/heroku_6cz96bd9' ||'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+var username = process.env.DB_USERNAME;
+var password = process.env.DB_PASSWORD;
+mongoose.connect("mongodb://" + username + ":" + password + "@ds117158.mlab.com:17158/todo" ||'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {mongoose};
