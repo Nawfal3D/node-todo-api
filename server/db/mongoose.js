16b593c4baae97f13280ce2dc775dd8db240674b
Nawfal Al Hashimy <nawfal@ecs-mac-mar-4.home>
2018-01-26 19:12:33 +0000

    setup app for heroku - corrected mongo db error using heroku payment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var username = 'dbconnect';
-var password = 'dbconnect';
-mongoose.connect("mongodb://" + username + ":" + password + "@ds117148.mlab.com:17148/heroku_6cz96bd9" ||'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+// var username = 'dbconnect';
+// var password = 'dbconnect';
+// mongoose.connect("mongodb://" + username + ":" + password + "@ds117148.mlab.com:17148/heroku_6cz96bd9" ||'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {mongoose};
