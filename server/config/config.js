ae6660e76639614f948838cada4389b05f20c436
Nawfal Al Hashimy <nawfal@ECS-Mac-Mar-4.local>
2018-02-08 17:00:12 +0000

    Added DELETE /users/me/token route

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env ==='development'){
-  process.env.PORT = 3040;
+  process.env.PORT = 3041;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env ==='test') {
   process.env.PORT = 3038;
